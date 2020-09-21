53648e86a79f9d86b26941c8856d0e082c9d1f49
alikahwaji <ali.kahwaji@nmit.ac.nz>
2020-09-22 09:41:21 +1200

    Added comments

--- scripts/index.js
+++ scripts/index.js
@@ -14,16 +14,20 @@ function createWindow() {
 /**
  * In the code above, we have used the window object of the existing window to create a new window using the open() method. 
  * In the open() method we can provide the URL to be opened in the new window(we can keep it blank as well), name of the window, 
- * the width and the height of the window to be created.
+the width and the height of the window to be created.
 
 Following is the syntax for the window object open() method:
 
-let newWindow = window.open(url, windowName, [windowFeatures]);
-We can provide, as many properties as we want, while creating a new window.
+* let newWindow = window.open(url, windowName, [windowFeatures]);
 
-When the open() method is executed, it returns the reference of the window object for the new window created, which you can assign to a variable, like we have done in the code above. We have assigned the value returned by the window.open() method to the win variable.
+* We can provide, as many properties as we want, while creating a new window.
 
-The we have used the win variable to access the new window, like getting the name of the window, getting location of the window which opened the new window etc.
+* When the open() method is executed, it returns the reference of the window object for the new window created, 
+which you can assign to a variable, like we have done in the code above. 
+* We have assigned the value returned by the window.open() method to the win variable.
+
+* The we have used the win variable to access the new window, like getting the name of the window, 
+getting location of the window which opened the new window etc.
 
 There are many properties and methods for the window object, which we have listed down below.
  */
